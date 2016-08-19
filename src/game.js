13510f56fa08ef3f242fc136782f9154f7863a1b
makro <imake@mbnet.fi>
2016-08-19 23:12:14 +0300

    Project initial setup

---
+++ src/game.js
@@ -0,0 +1,10 @@
+/*
+ *  Bounce Game Demo
+ *  by Marko Kallinki, 19.08.2016
+ *  Copyright - Do not distribute
+ */
+
+// Test project setup
+var test = _.range(0, 30, 5);
+document.getElementById("Content").innerHTML = "Js works : " + test;
+console.log ("JS is up and running");
