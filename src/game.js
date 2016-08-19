ccb23b27b6b53ec30088476dba9d488db882092b
makro <imake@mbnet.fi>
2016-08-20 00:33:07 +0300

    Create canvas

--- src/game.js
+++ src/game.js
@@ -4,7 +4,85 @@
  *  Copyright - Do not distribute
  */
 
-// Test project setup
-var test = _.range(0, 30, 5);
-document.getElementById("Content").innerHTML = "Js works : " + test;
-console.log ("JS is up and running");
+// -------------------------------------------------------------------------
+// Setup canvas variables
+var htmlCanvas = document.getElementById('htmlcanvas'),
+    canvasW = htmlCanvas.width,
+    canvasH = htmlCanvas.height,
+    canvas = htmlCanvas.getContext('2d'),
+    mouseX = -1, mouseY = -1, mouseDown = false,
+    keyPressed = null;
+
+function getMousePos(canvas, evt) {
+    var rect = canvas.getBoundingClientRect();
+    return {
+        x: evt.clientX - rect.left,
+        y: evt.clientY - rect.top
+    };
+}
+
+htmlCanvas.addEventListener("mousemove",
+    function(evt) {
+        var mousePos = getMousePos(htmlCanvas, evt);
+        mouseX = mousePos.x;
+        mouseY = mousePos.y;
+    }, false);
+
+htmlCanvas.addEventListener("mousedown",
+    function(evt) {
+        mouseDown = true;
+    }, false);
+
+htmlCanvas.addEventListener("mouseup",
+    function(evt) {
+        mouseDown = false;
+    }, false);
+
+htmlCanvas.addEventListener("keydown",
+    function(evt) {
+        // WASD and Arrow keys 
+        if      (evt.keyCode == 87 || evt.keyCode == 38) { keyPressed = "Up"; }
+        else if (evt.keyCode == 83 || evt.keyCode == 40) { keyPressed = "Down"; }
+        else if (evt.keyCode == 65 || evt.keyCode == 37) { keyPressed = "Left"; }
+        else if (evt.keyCode == 68 || evt.keyCode == 39) { keyPressed = "Right"; }
+        else if (evt.keyCode == 32) { keyPressed = "Spacebar"; }
+        else if (evt.keyCode == 16) { keyPressed = "Shift"; }
+        else if (evt.keyCode == 17) { keyPressed = "Control"; }
+        else if (evt.keyCode == 27) { keyPressed = "Escape"; }
+    }, true);
+
+htmlCanvas.addEventListener("keyup",
+    function(evt) {
+        keyPressed = null;
+    }, true);
+
+// -------------------------------------------------------------------------
+// Draw functions
+function drawEmptyScreen() {
+    canvas.fillStyle = "black";
+    canvas.fillRect(0, 0, canvasW, canvasH);
+    canvas.fillStyle = "white";
+    canvas.fillRect(1, 1, canvasW-2, canvasH-2);
+}
+
+function drawTestData() {
+    canvas.font = "16px Courier New";
+    canvas.fillStyle = "black";
+    canvas.fillText("Canvas", 130, 110);
+
+    canvas.fillText("Mouse X:" + mouseX + " Y:" + mouseY, 80, 140);
+    if (mouseDown) {
+        canvas.fillText("Mouse pressed", 100, 160);
+    }
+    if (keyPressed) {
+        canvas.fillText("Key " + keyPressed + " pressed", 90, 180);
+    }
+}
+
+// -------------------------------------------------------------------------
+// Screen redraw loop
+setInterval(function() {
+    drawEmptyScreen();
+    drawTestData();
+}, 20 /* milliseconds -> 50Hz */); 
+
